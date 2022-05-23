0c8a9461fefcfd5ed6d7d1bc378e92e4f13ca04e
Denise Rosegg <denise1987@gmail.com>
2022-05-23 21:30:44 +0300

    Adds Board component and starts implementing game functionality

---
+++ src/components/Board/index.js
@@ -0,0 +1,84 @@
+import React, {useCallback, useEffect, useState} from "react";
+import {cloneDeep} from "lodash";
+
+const initialBoard = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
+const baseValue = 2;
+
+function getRandomNumber(maxLimit = 4) {
+    let rand = Math.random() * maxLimit;
+    rand = Math.floor(rand); // 99
+    return rand;
+}
+
+function boardHasEmpty(board) {
+    return board.find(row => row.find(square => square === 0));
+}
+
+function Board() {
+    const [boardValues, setBoardValues] = useState(initialBoard);
+    const [isFull, setIsFull] = useState(false);
+
+    const updateBoard = useCallback((board = boardValues) => {
+        // if (!boardHasEmpty(board)) {setIsFull(true); return board};
+        const x = getRandomNumber();
+        const y = getRandomNumber();
+
+        if (board[x][y] === 0) {
+            const newBoard = cloneDeep(board);
+            newBoard[x][y] = baseValue;
+            setBoardValues(newBoard);
+        } else (updateBoard(board));
+
+    }, []);
+
+    function getSquareDisplayValue(value) {
+        return value > 0 ? value : '';
+    }
+
+    const shiftUp = useCallback(() => {
+        const newBoard = cloneDeep(boardValues);
+        newBoard.map((row, rowIndex) => {
+            if (rowIndex === 0) return [...row];
+
+            return row.map((square, index) => {
+                if (square === 0) return 0;
+
+                let nextEmpty = rowIndex;
+                for (let i = rowIndex - 1; i >= 0; i--) {
+                    if (newBoard[i][index] === 0) nextEmpty = i;
+                }
+
+                const aboveEmpty = nextEmpty - 1;
+                const aboveEmptyExists = aboveEmpty >= 0;
+                if (nextEmpty !== rowIndex || aboveEmptyExists ) {
+
+                    if (aboveEmptyExists && newBoard[aboveEmpty][index] === square) {
+                        newBoard[aboveEmpty][index] = square * 2;
+                        newBoard[rowIndex][index] = 0;
+                    } else {
+                        newBoard[nextEmpty][index] = square;
+                        newBoard[rowIndex][index] = 0;
+                    }
+                }
+
+            });
+        });
+
+        updateBoard(newBoard);
+
+    }, [boardValues]);
+
+    useEffect(() => {
+        updateBoard();
+    }, []);
+
+    return (
+        <div className="board" onClick={shiftUp}>
+            {boardValues.map((row, i) => <div className="row" key={i}>
+                {row.map((square, j) => <div className="square" key={j}>{getSquareDisplayValue(square)}</div>)}
+            </div>)}
+        </div>
+    );
+}
+
+export default Board;
